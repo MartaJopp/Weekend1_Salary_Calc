496da6b75cd04b5b8ff88c60ee4f82357aa90791
Marta Jopp <martapeterson@gmail.com>
2017-10-06 16:00:52 -0500

    button connected

--- client.js
+++ client.js
@@ -1,2 +1,18 @@
 //client.js sourced
 console.log('JS Sourced');
+
+$(document).ready(readyNow);
+
+//once html loaded - run this
+function readyNow(){
+$('button').on('click', submitButton);
+
+
+}//end readyNow function
+
+
+
+
+function submitButton(){
+  console.log ('button clicked');
+}//end submitButton function
