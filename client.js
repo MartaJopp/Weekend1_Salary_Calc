b07b97bf07adb01bb4b4635711649b0851a1dcf2
Marta Jopp <martapeterson@gmail.com>
2017-10-06 22:45:16 -0500

    salary adding correct

--- client.js
+++ client.js
@@ -10,9 +10,24 @@ $('button').on('click', submitButton);
 
 }//end readyNow function
 
+var numOfEmployees = 0;
+var totalAnnualSalary = 0;
 
-
-
+var $avgMonthlySalaryCost = 0;
 function submitButton(){
   console.log ('button clicked');
+  console.log ($('.employee').val());
+  var empSalary = parseInt($('.employee').val());
+  $('.content').append(empSalary);
+  totalAnnualSalary = (totalAnnualSalary + empSalary);
+  $('#Money').text(totalAnnualSalary);
+  console.log (totalAnnualSalary);
 }//end submitButton function
+$(this).text();
+var color = $(this).text(); // NOTE: The button text is 'red' & 'yellow'
+// Get the current block count from the HTML and convert to an Integer
+var currentBlockCount = parseInt($('#' + color + 'BlockCount').text());
+// Add one
+currentBlockCount += 1;
+// Set the block count to be the new number
+$('#' + color + 'BlockCount').text(currentBlockCount);
