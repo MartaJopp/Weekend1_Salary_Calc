6d8a42a80336c548869098ef72900cac2f264921
Marta Jopp <martapeterson@gmail.com>
2017-10-06 22:52:12 -0500

    monthly salary adding correct

--- client.js
+++ client.js
@@ -5,23 +5,24 @@ $(document).ready(readyNow);
 
 //once html loaded - run this
 function readyNow(){
-$('button').on('click', submitButton);
+$('button').on('click', submitButtonSalary);
 
 
 }//end readyNow function
 
 var numOfEmployees = 0;
-var totalAnnualSalary = 0;
+var totalMonthlyCosts = 0;
 
 var $avgMonthlySalaryCost = 0;
-function submitButton(){
+function submitButtonSalary(){
   console.log ('button clicked');
   console.log ($('.employee').val());
   var empSalary = parseInt($('.employee').val());
+  var empMonthlySalary = Math.round(empSalary/12);
   $('.content').append(empSalary);
-  totalAnnualSalary = (totalAnnualSalary + empSalary);
-  $('#Money').text(totalAnnualSalary);
-  console.log (totalAnnualSalary);
+  totalMonthlyCosts = (totalMonthlyCosts + empMonthlySalary);
+  $('#Money').text(totalMonthlyCosts);
+  console.log (totalMonthlyCosts);
 }//end submitButton function
 $(this).text();
 var color = $(this).text(); // NOTE: The button text is 'red' & 'yellow'
